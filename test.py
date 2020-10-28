27b81604b29292345bf46eb5df8af117533c27a1
kitatakisan-KM <kitatakisan42@gmail.com>
2020-10-28 23:05:44 +0900

    test.py delete

--- test.py
+++
@@ -1,9 +0,0 @@
-import os
-from selenium import webdriver
-userdata_dir = 'UserData'  # カレントディレクトリの直下に作る場合
-os.makedirs(userdata_dir, exist_ok=True)
-
-options = webdriver.ChromeOptions()
-options.add_argument('--user-data-dir=' + userdata_dir)
-
-driver = webdriver.Chrome(options=options)
